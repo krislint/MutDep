dc6b29e49bd5b4789b4fc4cbb111bff3bbc74f9e
krislint <wrisboy@gmail.com>
2020-06-11 21:28:29 +0800

    Initial commit

---
+++ web/src/main/java/com/moelyon/mutdep/web/controller/CoffeeController.java
@@ -0,0 +1,22 @@
+package com.moelyon.mutdep.web.controller;
+
+import com.moelyon.common.model.Coffee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author moelyon
+ */
+@RestController
+@RequestMapping("/coffee")
+public class CoffeeController {
+
+    @GetMapping("")
+    public Coffee getCoffee(){
+        return Coffee.builder()
+                .name("qaq")
+                .price(223d)
+                .build();
+    }
+}
